885fee2f13cc8926b6255f584cd18c7ae3139445
simone <simone.l.silva@parceiros.nos.pt>
2025-02-16 01:53:10 +0000

    fix: error profile image

--- src/components/Image.vue
+++ src/components/Image.vue
@@ -1,7 +1,7 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <img
-    src="`@/assets/profile.png`"
+    :src="profileImage"
     alt="`Simone`"
     :style="{ width: `${props.width}px`, height: `${props.height}px` }"
   />
@@ -9,6 +9,7 @@
 
 <script setup>
 import { defineProps } from 'vue'
+import profileImage from '@/assets/profile.png'
 
 const props = defineProps({
   width: String,
